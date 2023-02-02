01f38012b083b1dd355f729285b6f260b580ca6b
Student User <student@workstation.lab.example.com>
2023-02-02 13:07:43 -0500

    version 4

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,6 +1,9 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
+print "</br>";
 print "¿Viene un cambio automático en la aplicacion?\n";
 print "</br>";
-print "Mas cosas";
+print "Mas cosas. Ejecutar lo siguiente para que se actualice la aplicacion";
+print "</br>";
+print "oc start-build php-helloworld";
 ?>
